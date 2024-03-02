ccc6094ff66b1d2bdd4699d7012b17f22c6d42b8
zaru <zarutofu@gmail.com>
2024-03-02 10:15:31 +0900

    テスト実行時にだけ、パスワード認証を有効化

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,29 @@
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { type Provider } from "@auth/core/providers";
 
 const providers: Provider[] = [GitHub];
+
+if (process.env.APP_ENV === "test") {
+  providers.push(
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // 固定のユーザデータをセッションとして返す
+        return {
+          id: "1",
+          name: "dummy user",
+          email: "dummy@example.com",
+        };
+      },
+    }),
+  );
+}
 
 export const config = {
   theme: {
